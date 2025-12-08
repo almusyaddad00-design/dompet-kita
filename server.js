92a6fe8f62f5999db42047fcf85461a0fdf694be
Alfan Musyaddad <almusyaddad00@gmail.com>
2025-12-08 18:31:54 +0700

    Siap deploy ke Vercel

--- server.js
+++ server.js
@@ -96,4 +96,8 @@ const ledgerRoutes = require('./routes/ledgerRoutes');
 app.use('/dashboard', checkAuth, ledgerRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server berjalan di http://localhost:${PORT}`));
+if (require.main === module) {
+    app.listen(PORT, () => console.log(`Server berjalan di http://localhost:${PORT}`));
+}
+
+module.exports = app;
